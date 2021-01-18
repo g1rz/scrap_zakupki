9bfd65043f553a8edbb07d3cb4f4b374992d14f5
Sergey Peshkov <zloi.g1rz@gmail.com>
2021-01-19 01:53:09 +0800

    Начал промисы

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const fs = require('fs');
 
 let i = 1;
 
+let countPages = 0;
+
 let promise = new Promise((resolve) => {
     let savedData = [];
     osmosis
         .get(link)
-        // .paginate('.paginator .pages > a[href]', 5)
         .find('.search-registry-entry-block')
         .set({
             orderID: '.registry-entry__header-mid__number a',
@@ -22,14 +23,21 @@ let promise = new Promise((resolve) => {
             console.log(data);
             savedData.push(data);
         })
+        .find('.pages')
+        .set({
+            countPages: 'li:last-of-type',
+        })
+        .data(function (data) {
+            console.log(data);
+        })
         .log(console.log) // включить логи
         .error(console.error) // на случай нахождения ошибки
         .done(function () {
             resolve(savedData);
-            // fs.writeFile('data.json', JSON.stringify(savedData, null, 4), function (err) {
-            //     if (err) console.error(err);
-            //     else console.log('Data Saved to data.json file');
-            // });
+            fs.writeFile('data.json', JSON.stringify(savedData, null, 4), function (err) {
+                if (err) console.error(err);
+                else console.log('Data Saved to data.json file');
+            });
         });
 });
 
